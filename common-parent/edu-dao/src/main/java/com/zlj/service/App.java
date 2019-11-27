3ba86a3d7f3a0487ced53b4c087d6d6a8a00e3d4
zlj <junstar106@gmail.com>
2019-11-27 14:41:25 +0800

    first push ssm

---
+++ common-parent/edu-dao/src/main/java/com/zlj/service/App.java
@@ -0,0 +1,13 @@
+package com.zlj.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
